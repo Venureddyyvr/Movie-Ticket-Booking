a699f64bbc3e7fdab5cc3184ce91bf4618e2fff6
Ali <2085836@cognizant.com>
2022-11-16 13:55:08 +0530

    added properties in models

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -2,5 +2,11 @@
 {
     public class Cart
     {
+        public int Id { get; set; }
+        public string SeatNumber { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+        public int MovieId { get; set; }
     }
 }
